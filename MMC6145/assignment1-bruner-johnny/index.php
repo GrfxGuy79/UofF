183bcb3525706b23eeed0516a087811955c3f20f
Johnny Bruner <johnny@johnnybrunerdesigns.com>
2022-05-25 03:37:17 -0700

    Added form

--- MMC6145/assignment1-bruner-johnny/index.php
+++ MMC6145/assignment1-bruner-johnny/index.php
@@ -4,10 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <title>Assignment 1</title>
 </head>
 <body>
     
+<header></header>
+
+<main>
+<!-- FORM -->
+<form action="" method="POST">
+    <input type="text" name="name" class="name">
+    <input type="email" name="email" class="email">
+    <input type="tel" name="phone" class="phone">
+
+    <!-- Radio -->
+
+    <textarea name="desc" class="desc" cols="30" rows="10"></textarea>
+
+    <!-- Dropdown -->
+    
+</form>
+</main>
+
+<footer></footer>
 </body>
 </html>
