64ad9b71453c7b19a7cb6c2c371cc13068f6b095
Johnny Bruner <johnny@johnnybrunerdesigns.com>
2022-05-30 02:45:02 -0700

    Coded results page

--- MMC6145/assignment1-bruner-johnny/index.php
+++ MMC6145/assignment1-bruner-johnny/index.php
@@ -1,104 +1,107 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- CSS -->
-<link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
 
     <title>Assignment 1</title>
 </head>
+
 <body>
-    
-<header></header>
 
-<main>
-<!-- PASSWORD FORM -->
-<form action="" method="post" class="password-form">
-<ul>
-    <li class="pass-field"> 
-            <label for="password" class="pass-label">Password:</label>   
-            <input type="password" name="password" class="password" value="">
-            <label for="password" class="phone-ex">Please enter the password and press Enter</label>
-</li>
-</ul>
-</form>
+    <header></header>
+
+    <main>
+        <!-- PASSWORD FORM -->
+        <form action="" method="post" class="password-form">
+            <ul>
+                <li class="pass-field">
+                    <label for="password" class="pass-label">Password:</label>
+                    <input type="password" name="password" class="password" value="">
+                    <label for="password" class="phone-ex">Please enter the password and press Enter</label>
+                </li>
+            </ul>
+        </form>
 
 
-<?php
-if(isset($_POST['password']) && $_POST['password']=='bruner'){?>
-    <div class="form-title">
-        <h2>Please fill out the form below</h2>
-    </div>
+        <?php
+if(isset($_POST['password']) && $_POST['password'] == 'bruner'){?>
+        <div class="form-title">
+            <h2>Please fill out the form below</h2>
+        </div>
 
-<!-- FORM -->
-<form action="results.php" method="POST">
-    
+        <!-- FORM -->
+        <form action="results.php" method="POST">
 
-    <ul>
-        <!-- INPUT -->
-        <li class="name-field">
-            <label for="name" class="name-label">Name:</label>
-<input type="text" name="name" class="name" value="">
-        </li>
-        
-        <li class="email-field">   
-            <label for="email" class="email-label">Email:</label>
-            <input type="email" name="email" class="email" value="">
-</li>
-        <li class="phone-field">   
-            <label for="phone" class="phone-label">Phone Number:</label>
-            <input type="tel" name="phone" class="phone" value="" size="10" maxlength="10">
-            <label for="phone" class="phone-ex">##########</label>
-</li>
 
-    <!-- RADIO -->
-    <label for="degree" class="degree-label">What Degree Are You Seeking?</label>
-<li class="degree-field">
-    <input type="radio" name="degree-a" class="degree" value="Associates">
-       <label for="radio-label-a">Associates</label><br>
- <input type="radio" name="degree-b" class="degree" value="Bachelors">
-     <label for="radio-label-b">Bachelors</label><br>
-<input type="radio" name="degree-m" class="degree" value="Masters">
-        <label for="radio-label-m">Masters</label><br>
-<input type="radio" name="degree-phd" class="degree" value="PhD">
-       <label for="radio-label-p">PhD</label>
-</li>
-<!-- DROPDOWN -->
-<label for="year" class="year-label">What Year Are You Currently In?</label>
-<li class="year-field">
-<select name="year" class="year">
-    <option value="Freshman">Freshman</option>
-    <option value="Freshman">Junior</option>
-    <option value="Freshman">Sophomore</option>
-    <option value="Freshman">Senior</option>
-</select>
-</li>
+            <ul>
+                <!-- INPUT -->
+                <li class="name-field">
+                    <label for="name" class="name-label">Name:</label>
+                    <input type="text" name="name" class="name" value="">
+                </li>
 
-<!-- TEXTAREA -->
-<label for="comments" class="comments-label">Any Comments You Would Like To Add?</label>
-<li class="comments-field">
-    <textarea name="comments" class="comments" cols="30" rows="10"></textarea>
-</li>
-    
-<!-- SUBMIT -->
-<li class="submit-field">
-    <input type="submit" name="submit" class="submit" value="Submit">
-</li>
+                <li class="email-field">
+                    <label for="email" class="email-label">Email:</label>
+                    <input type="email" name="email" class="email" value="">
+                </li>
+                <li class="phone-field">
+                    <label for="phone" class="phone-label">Phone Number:</label>
+                    <input type="tel" name="phone" class="phone" value="" size="10" maxlength="10">
+                    <label for="phone" class="phone-ex">##########</label>
+                </li>
 
-    </ul>
-</form>
-<?php 
+                <!-- RADIO -->
+                <label for="degree" class="degree-label">What Degree Are You Seeking?</label>
+                <li class="degree-field">
+                    <input type="radio" name="degree-a" class="degree" value="Associates">
+                    <label for="radio-label-a">Associates</label><br>
+                    <input type="radio" name="degree-b" class="degree" value="Bachelors">
+                    <label for="radio-label-b">Bachelors</label><br>
+                    <input type="radio" name="degree-m" class="degree" value="Masters">
+                    <label for="radio-label-m">Masters</label><br>
+                    <input type="radio" name="degree-p" class="degree" value="PhD">
+                    <label for="radio-label-p">PhD</label>
+                </li>
+                <!-- DROPDOWN -->
+                <label for="year" class="year-label">What Year Are You Currently In?</label>
+                <li class="year-field">
+                    <select name="year" class="year">
+                        <option value="Freshman">Freshman</option>
+                        <option value="Sophomore">Sophomore</option>
+                        <option value="Junior">Junior</option>
+                        <option value="Senior">Senior</option>
+                    </select>
+                </li>
+
+                <!-- TEXTAREA -->
+                <label for="comments" class="comments-label">Any Comments You Would Like To Add?</label>
+                <li class="comments-field">
+                    <textarea name="comments" class="comments" cols="30" rows="10"></textarea>
+                </li>
+
+                <!-- SUBMIT -->
+                <li class="submit-field">
+                    <input type="submit" name="submit" class="submit" value="Submit">
+                </li>
+
+            </ul>
+        </form>
+        <?php 
 }elseif (isset($_POST['password']) && $_POST['password']!='bruner'){
     $errormsg = "Sorry That Password Is Incorrect, Please Try Again!";
     echo '<h3 class="errormsg">'.$errormsg.'</h3>';
 }
 ?>
-    
-</main>
 
-<footer></footer>
+    </main>
+
+    <footer></footer>
 </body>
+
 </html>
--- MMC6145/assignment1-bruner-johnny/results.php
+++ MMC6145/assignment1-bruner-johnny/results.php
@@ -2,14 +2,78 @@
 
 // FORM VARIABLES
 $name = $_POST['name'];
-$password = $_POST['password'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
-$associates = $_POST['degree-a'];
-$bachelors = $_POST['degree-b'];
-$masters = $_POST['degree-m'];
-$phd = $_POST['degree-p'];
 $year = $_POST['year'];
 $comments = $_POST['comments'];
+$degree;
 
+// GET DEGREE
+if(isset($_POST['degree-a'])){
+    $degree = "Associates";
+}elseif(isset($_POST['degree-b'])){
+    $degree = "Bachelors";
+}elseif(isset($_POST['degree-m'])){
+    $degree = "Masters";
+}elseif(isset($_POST['degree-p'])){
+    $degree = "Doctorite";
+}
+
+// GET YEAR
+if(isset($_POST['year']) && $_POST['year'] == 'Freshman'){
+    $current_year ="As a Freshman, it might have only just begun, but the knowledge you will obtain will stay with you the rest of life.";
+}elseif(isset($_POST['year']) && $_POST['year'] == 'Sophomore'){
+    $current_year ="One year down Sophomore, keep your mind focused and your eye on the prize!";
+}elseif(isset($_POST['year']) && $_POST['year'] == 'Junior'){
+    $current_year ="OK Junior, how good does it feel to know you are more than half way through your journey? YOU CAN DO THIS!";
+}elseif(isset($_POST['year']) && $_POST['year'] == 'Senior'){
+    $current_year ="YOU DID IT SENIOR!! You are almost cross that finish line!";
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>Assignment 1</title>
+</head>
+
+<body>
+
+    <header></header>
+
+    <main>
+        <h2 class="hello">Hello <?php echo $name;?></h2>
+
+        <div class="contact-info">
+            <h3>Here is your contact information:</h3>
+            <p>
+                Email Address: <strong><?php echo $email;?></strong></br>
+                Phone Number: <strong><?php echo $phone;?></strong>
+            </p>
+        </div>
+
+        <div class="degree-info">
+            <h3>Here is your educational information:</h3>
+            You should be very proud of yourself for working towards your <?php echo $degree;?> Degree.</br>
+            <?php echo $current_year;?>
+        </div>
+
+        <div class="comments-info">
+            <h3>Your added comments:</h3>
+
+            <?php echo $comments;?>
+        </div>
+    </main>
+
+    <footer></footer>
+</body>
+
+</html>
