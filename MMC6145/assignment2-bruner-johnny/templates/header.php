79cbb7e68759aa5c29c43792aaf3a49d5bd1bc4f
Johnny Bruner <johnny@johnnybrunerdesigns.com>
2022-05-30 23:20:32 -0700

    Coded header

---
+++ MMC6145/assignment2-bruner-johnny/templates/header.php
@@ -0,0 +1,25 @@
+<?php
+
+include './includes/functions.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>My Site Of Quotes</title>
+    
+</head>
+<body>
+    
+<div class="container">
+    <h1><a href="index.php">My Site Of Quotes</a></h1>
+
+    <!-- BODY CONTENT -->
