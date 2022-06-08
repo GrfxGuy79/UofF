00b70655550f774cd720254428fe205428f3d73b
Johnny Bruner <johnny@johnnybrunerdesigns.com>
2022-06-08 02:26:36 -0700

    Added Bootstrap CDN

--- MMC6145/assignment2-bruner-johnny/templates/footer.php
+++ MMC6145/assignment2-bruner-johnny/templates/footer.php
@@ -1,4 +1,7 @@
 </div>
 
+<!-- BOOTSTRAP -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
--- MMC6145/assignment2-bruner-johnny/templates/header.php
+++ MMC6145/assignment2-bruner-johnny/templates/header.php
@@ -11,14 +11,17 @@ include './includes/functions.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <!-- CSS -->
     <link rel="stylesheet" href="./css/style.css">
 
     <title>My Site Of Quotes</title>
-    
+
 </head>
 <body>
-    
+
 <div class="container">
     <h1><a href="index.php">My Site Of Quotes</a></h1>
 
