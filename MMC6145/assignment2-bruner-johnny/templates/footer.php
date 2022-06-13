1fd6f775d2b6efb78640fc4233736e234b21e24e
Johnny Bruner <johnny@johnnybrunerdesigns.com>
2022-06-12 21:18:19 -0700

    Add tooltip JS

--- MMC6145/assignment2-bruner-johnny/templates/footer.php
+++ MMC6145/assignment2-bruner-johnny/templates/footer.php
@@ -8,5 +8,10 @@ Content &copy; <?php echo date('Y'); ?>
 <!-- BOOTSTRAP -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 
+<script>
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+</script>
+
 </body>
 </html>
