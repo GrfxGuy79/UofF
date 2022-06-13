8195b7f153e834bbdbb53ff5b68d3649a9c6fb45
Johnny Bruner <johnny@johnnybrunerdesigns.com>
2022-06-12 19:56:15 -0700

    Added Random links

---
+++ MMC6145/assignment2-bruner-johnny/index.php
@@ -0,0 +1,42 @@
+<?php
+
+include './templates/header.php';
+
+//
+if (isset($_GET['random'])) {
+    // SHOW RANDOM QUOTE
+    $query = 'SELECT * FROM quotes ORDER BY RAND() DESC LIMIT 1';
+} elseif (isset($_GET['favorite'])) {
+    // SHOW RANDOM FAVORITE QUOTE
+    $query = 'SELECT * FROM quotes WHERE favorite=1 ORDER BY RAND() DESC LIMIT 1';
+} else {
+    // SHOW MOST RECENT QUOTE
+    $query = 'SELECT * FROM quotes ORDER BY date_entered DESC LIMIT 1';
+}
+
+if ($result = mysqli_query($dbc, $query)) {
+    $row = mysqli_fetch_array($result);
+
+    // DISPLAY RESULTS
+    echo '<div><blockquote>' . $row['quote'] . '</blockquote>- ' . $row['source'] . '';
+    // CHECK IF FAVORITE
+    if ($row['favorite'] == 1) {
+        echo '<strong>Favorite</strong>';
+    }
+    echo '</div>';
+
+    if (is_admin()) {
+        // ADMIN LINKS
+        echo '<div><p>Quote Admin: <a href="edit_quote.php?id=' . $row['id'] . '">Edit</a>
+        <a href="delete_quote.php?id=' . $row['id'] . '">Delete</a></p></div>';
+    }
+} else {
+    echo '<p class="error">Could not retrieve the data because ' . mysqli_error($dbc) . ' </p>';
+    echo '<p>The query being ran is: ' . $query . '</p>';
+}
+
+mysqli_close($dbc);
+
+echo '<p><a href="index.php">Latest</a> | <a href="index.php?random=true">Random</a> | <a href="index.php?favorite=true">Random Favorite</a></p>';
+
+include './templates/footer.php';
