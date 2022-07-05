7f01577668e9efc4039d38a67a504195c1df81b7
Johnny Bruner <johnny@johnnybrunerdesigns.com>
2022-07-04 17:15:33 -0700

    Added featured images

--- MMC6145/wp-bruner-johnny/wp-content/themes/booking-theme/functions.php
+++ MMC6145/wp-bruner-johnny/wp-content/themes/booking-theme/functions.php
@@ -7,15 +7,22 @@ ADD STYLESHEETS AND JS FILES
 function custom_theme_scripts()
 {
     // BOOTSTRAP CSS
-    wp_enqueue_style('bs_style', get_stylesheet_directory_uri() . '/css/bootstrap.css');
+    wp_enqueue_style('bs-style', get_stylesheet_directory_uri() . '/css/bootstrap.css');
 
     // MAIN STYLE CSS
     wp_enqueue_style('main-styles', get_stylesheet_uri());
 
     // BOOTSTRAP JS
-    wp_enqueue_script('bs_js', get_stylesheet_directory_uri() . '/js/bootstrap.js');
+    wp_enqueue_script('bs-js', get_stylesheet_directory_uri() . '/js/bootstrap.js');
 
     // MAIN JS
-    wp_enqueue_script('bs_js', get_stylesheet_directory_uri() . '/js/scripts.js');
+    wp_enqueue_script('bs-js', get_stylesheet_directory_uri() . '/js/scripts.js');
 
 }
+// ADD ACTION TO SITE
+add_action('wp_enqueue_scripts', 'custom_theme_scripts');
+
+/*------------------------------
+ADD FEATURED IMAGES TO SITE
+------------------------------*/
+add_theme_support('post-thumbnails');
