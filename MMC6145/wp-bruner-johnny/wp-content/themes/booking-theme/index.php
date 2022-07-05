eb9a3dfbef73234990193c75c4f3f32893325c9a
Johnny Bruner <johnny@johnnybrunerdesigns.com>
2022-07-04 21:46:50 -0700

    Added code to index,page, single

--- MMC6145/wp-bruner-johnny/wp-content/themes/booking-theme/index.php
+++ MMC6145/wp-bruner-johnny/wp-content/themes/booking-theme/index.php
@@ -1,5 +1,27 @@
 <?php get_header();?>
 
-<h1>HERE WE GO</h1>
+<main class="container">
+    <?php
 
+if (have_posts()) {
+    while (have_posts()) {
+        the_post();?>
+
+    <div class="individual-post">
+        <div class="featured-image">
+            <?php the_post_thumbnail('thumbnail');?>
+        </div><!-- featured image -->
+
+        <div class="text-container">
+            <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+            <p class="excerpt">
+                <?php echo get_the_excerpt(); ?>
+            </p>
+        </div><!-- text container -->
+    </div><!-- individual post -->
+    <?php
+}
+}
+?>
+</main>
 <?php get_footer();?>
