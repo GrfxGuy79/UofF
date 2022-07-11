b5fa86aa5dcd25125e8afdc9f1abf05d1fe35a96
Johnny Bruner <johnny@johnnybrunerdesigns.com>
2022-07-10 23:31:53 -0700

    Added footer menu

--- MMC6145/assignment4-bruner-johnny/wp-content/themes/jbtheme/footer.php
+++ MMC6145/assignment4-bruner-johnny/wp-content/themes/jbtheme/footer.php
@@ -1,2 +1,48 @@
+<footer>
+    <div class="container">
+        <div class="row">
+
+            <!-- LEFT FOOTER MENU -->
+            <div class="col-md-4">
+                <?php
+if (has_nav_menu('footer-left')) {?>
+                <nav class="footer-menu">
+                    <?php wp_nav_menu(array(
+    'theme_location' => 'footer-left',
+));
+    ?>
+                </nav>
+                <?php } else {echo "No Menu Has Been Set!";}?>
+            </div>
+
+            <!-- MIDDLE FOOTER MENU -->
+            <div class="col-md-4">
+                <?php
+if (has_nav_menu('footer-middle')) {?>
+                <nav class="footer-menu">
+                    <?php wp_nav_menu(array(
+    'theme_location' => 'footer-middle',
+));
+    ?>
+                </nav>
+                <?php } else {echo "No Menu Has Been Set!";}?>
+            </div>
+
+            <!-- RIGHT FOOTER MENU -->
+            <div class="col-md-4">
+                <?php
+if (has_nav_menu('footer-right')) {?>
+                <nav class="footer-menu">
+                    <?php wp_nav_menu(array(
+    'theme_location' => 'footer-right',
+));
+    ?>
+                </nav>
+                <?php } else {echo "No Menu Has Been Set!";}?>
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
+
 </html>
