3ead822b13b753066914f4ba6af7100efb14db6d
Johnny Bruner <johnny@johnnybrunerdesigns.com>
2022-07-13 01:03:51 -0700

    Style home page

--- MMC6145/assignment4-bruner-johnny/wp-content/themes/jbtheme/index.php
+++ MMC6145/assignment4-bruner-johnny/wp-content/themes/jbtheme/index.php
@@ -1,2 +1,37 @@
 <?php get_header();?>
+
+<main class="container home-page">
+    <?php
+
+if (have_posts()) {
+    while (have_posts()) {
+        the_post();?>
+
+        <div class="individual-post">
+            <div class="row justify-content-center">
+            <div class="col-lg-2">
+                <div class="featured-image">
+                    <a href="<?php the_permalink();?>"><?php the_post_thumbnail('thumbnail');?></a>
+                </div>
+            </div>
+
+            <div class="col-lg-8">
+                <div class="text-container">
+                    <h2 class="post-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+                    <p class="excerpt">
+                        <?php echo get_the_excerpt(); ?>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+        <hr>
+    </div>
+
+    <?php
+}
+}
+?>
+</main>
+
 <?php get_footer();
