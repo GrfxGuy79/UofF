aa73d00fae0922e9c6e3399b90902777f8654e8b
Johnny Bruner <johnny@johnnybrunerdesigns.com>
2022-07-15 02:12:20 -0700

    Styled

---
+++ MMC6145/project-bruner-johnny/wp-content/themes/bookingtheme/404.php
@@ -0,0 +1,23 @@
+<?php get_header();?>
+
+<div class="container">
+    <section class="page-404">
+        <div class="row">
+            <div class="col-sm-12">
+<div class="col-sm-10 col-sm-offset-1 text-">
+    <div class="four-zero-four-bg">
+        <h2 class="text-center">404</h2>
+    </div>
+
+    <div class="content-box-404">
+        <h2>Looks like you're lost</h2>
+        <p>The page you are looking for is not available!</p>
+        <a href="<?php echo get_home_url(); ?>" class="link-404">Go Home</a>
+    </div>
+</div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<?php get_footer();?>
